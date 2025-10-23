17c081bdb2319a3899a4c442bd0de17cc57dbe40
Aril Apria Susanto <awriel@gmail.com>
2025-10-23 09:03:02 +0700

    Merubah identitas nama POS

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -9,7 +9,7 @@
             <div class="copyright text-center my-auto">
                 <span>
                     <?php echo date('Y');?> - Sistem Penjualan Barang Berbasis Web |
-                    BY <b><a href="https://codekop.com" target="_blank">CODEKOP.COM</a></b>
+                    By <b><a href="https://codekop.com" target="_blank">CODEKOP.COM</a> Customized by <a href="https://awriel.com" target="_blank" >Awriel</a> </b>
                 </span>
             </div>
         </div>
